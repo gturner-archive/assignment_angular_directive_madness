2f92039588b9fd146104846fedfd423120fd6fce
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-23 10:00:29 -0700

    QuoteForm directive

--- quotes_ctrl.js
+++ quotes_ctrl.js
@@ -1,3 +1,14 @@
 directiveMadness.controller('QuotesCtrl', ['$scope', function($scope) {
 
 }]);
+
+directiveMadness.directive('quoteForm', function(){
+  return {
+    template: "<q>{{ message }}</q> -{{ author }}"
+    restrict: "E",
+    scope: {
+      message: "@",
+      author: "@"
+    },
+  };
+});
