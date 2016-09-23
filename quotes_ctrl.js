67038b566d6692439b41eb8c2306a0d570d94efd
Graham Turner <turnertgraham@gmail.com>
2016-09-23 13:22:12 -0400

    clearing forms

--- quotes_ctrl.js
+++ quotes_ctrl.js
@@ -1,14 +1,28 @@
 directiveMadness.controller('QuotesCtrl', ['$scope', function($scope) {
-
+  $scope.quotes = [];
+  $scope.updateQuotes = function(e, quote, author) {
+    e.preventDefault();
+    $scope.quotes.push(
+      {
+        quote: quote,
+        author: author
+      }
+    );
+  };
 }]);
 
 directiveMadness.directive('quoteForm', function(){
   return {
-    template: "<q>{{ message }}</q> -{{ author }}"
+    templateUrl: "directives/quote-form.html",
     restrict: "E",
-    scope: {
-      message: "@",
-      author: "@"
-    },
+    scope: true
+  };
+});
+
+directiveMadness.directive('quoteIndex', function(){
+  return {
+    templateUrl: "directives/quote-index.html",
+    restrict: "E",
+    scope: true
   };
 });
